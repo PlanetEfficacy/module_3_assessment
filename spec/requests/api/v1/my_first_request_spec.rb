5da09a698bdf46bfdb2186131f0e7b286620d577
PlanetEfficacy <jspevack@gmail.com>
2016-11-02 09:18:44 -0600

    set up feature test for api get items, created items factory, created items api route

--- spec/requests/api/v1/my_first_request_spec.rb
+++ spec/requests/api/v1/my_first_request_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Some API Request to a model, eg items post" do
-  it "gets all items" do
+RSpec.describe "Get request to /api/v1/items", :type => :request do
+  it "gets all items and their attributes as json" do
+    items = FactoryGirl.create_list(:item, 2)
+    get "/api/v1/items"
 
+    items = JSON.parse(response)
+
+    expect(response).to have_http_status(200)
+    expect(items.class).to eq(Array)
+    expect(items.count).to eq(2)
+    expect(items.first["id"]).to eq(Item.first.id)
+    expect(items.first["name"]).to eq(Item.first.name)
+    expect(items.first["description"]).to eq(Item.first.description)
+    expect(items.first["image_url"]).to eq(Item.first.image_url)
+    expect(items.first.keys).to_not include("created_at")
+    expect(items.first.keys).to_not include("updated_at")
   end
 end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,3 +1,9 @@
 FactoryGirl.define do
-  # <factories for each model go here>
+  factory :item do
+    name { Faker::Commerce.product_name }
+    description  { Faker::Hipster.sentence }
+    image_url { Faker::Avatar.image }
+    created_at { Faker::Time.backward(10, :evening) }
+    updated_at { Faker::Time.backward(5, :evening) }
+  end
 end
